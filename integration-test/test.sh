e22d3607d6c9ac993746e594e18cd5d56f66cd65
Joe Gordon <joe.gordon0@gmail.com>
2013-05-31 19:04:00 -0700

    Add integration test option to tox
    Take current version of hacking and run it on any
    review.openstack.org repository. By default nova is used
      tox -eintegration
    but you can pass in any project as an argument. For example:
      tox -eintegration -- openstack keystone
    
    This deletes the project repo after the test.
    
    Partially fixes bug 1186421
    
    Change-Id: I796b7c734459042b1a929afb73434cba62477365

---
+++ integration-test/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Usage: test.sh openstack keystone
+# Note: you can clone from a local file with REPO_ROOT=file:////~/path/to/repo
+set -x
+set -e
+REPO_ROOT=${REPO_ROOT:-https://review.openstack.org/p}
+if [[ -z "$2" ]]; then
+    org=openstack
+    project=nova
+else
+    org=$1
+    project=$2
+fi
+git clone $REPO_ROOT/$org/$project --depth=1
+cd $project
+set +e
+flake8
+cd ..
+rm -rf $project
