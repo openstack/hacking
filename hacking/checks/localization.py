4e300803f2ab44becbe737abdd1459aa026000c9
Joe Gordon <joe.gordon0@gmail.com>
2014-06-03 11:00:30 -0700

    Update localization checks to understand separate catalogs
    Make sure localization checks understand OpenStack's seperate message
    catalogs.
    
    https://wiki.openstack.org/wiki/LoggingStandards#Guidelines
    
    Change-Id: I958d615f6bc83f0d2fb50d897ce18b956a269d84

--- hacking/checks/localization.py
+++ hacking/checks/localization.py
@@ -42,8 +42,11 @@ def check_i18n():
         except GeneratorExit:
             return
 
-        if (token_type == tokenize.NAME and text == "_" and
-                not line.startswith('def _(msg):')):
+        if text == "def" and token_type == tokenize.NAME:
+            # explicitly ignore function definitions, as oslo defines these
+            return
+        if (token_type == tokenize.NAME and
+                text in ["_", "_LI", "_LW", "_LE", "_LC"]):
 
             while True:
                 token_type, text, start, _, _ = yield
@@ -80,7 +83,8 @@ def check_i18n():
                         "H702: Use bare string concatenation instead of +")
                 else:
                     raise LocalizationError(
-                        start, "H702: Argument to _ must be just a string")
+                        start, "H702: Argument to _, _LI, _LW, _LC, or _LE "
+                        "must be just a string")
 
             format_specs = FORMAT_RE.findall(format_string)
             positional_specs = [(key, spec) for key, spec in format_specs
@@ -96,13 +100,31 @@ def hacking_localization_strings(logical_line, tokens, noqa):
     r"""Check localization in line.
 
     Okay: _("This is fine")
+    Okay: _LI("This is fine")
+    Okay: _LW("This is fine")
+    Okay: _LE("This is fine")
+    Okay: _LC("This is fine")
     Okay: _("This is also fine %s")
     Okay: _("So is this %s, %(foo)s") % {foo: 'foo'}
     H701: _('')
+    Okay: def _(msg):\n    pass
+    Okay: def _LE(msg):\n    pass
+    H701: _LI('')
+    H701: _LW('')
+    H701: _LE('')
+    H701: _LC('')
     Okay: _('')  # noqa
     H702: _("Bob" + " foo")
+    H702: _LI("Bob" + " foo")
+    H702: _LW("Bob" + " foo")
+    H702: _LE("Bob" + " foo")
+    H702: _LC("Bob" + " foo")
     Okay: _("Bob" + " foo")  # noqa
     H702: _("Bob %s" % foo)
+    H702: _LI("Bob %s" % foo)
+    H702: _LW("Bob %s" % foo)
+    H702: _LE("Bob %s" % foo)
+    H702: _LC("Bob %s" % foo)
     # H703 check is not quite right, disabled by removing colon
     H703 _("%s %s" % (foo, bar))
     """
