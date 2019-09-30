3735d2d69370bf76c8130fffd55abcc70297483e
Masayuki Igawa <masayuki@igawa.io>
2019-09-30 15:20:33 +0900

    Add PDF documentation build
    This commit adds PDF documentation build target 'pdf-docs' that will
    build PDF versions of our docs.
    
    This is one of the Train community goals:
    
     https://governance.openstack.org/tc/goals/selected/train/pdf-doc-generation.html
    
    Change-Id: Iae6bc9ae92fe61385c424923eaad35dc449880e9
    Story: #2006070
    Task: #35464

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -55,7 +55,7 @@ htmlhelp_basename = '%sdoc' % project
 # [howto/manual]).
 latex_documents = [
         ('index',
-         '%s.tex' % project,
+         'doc-%s.tex' % project,
          u'%s Documentation' % project,
          u'OpenStack Foundation', 'manual'),
 ]
