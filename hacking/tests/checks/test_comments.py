ab5de186cf326c5ac95ed5f36a8dccff0a21f96c
Ilya Maximets <i.maximets@ovn.org>
2022-02-23 00:52:42 +0100

    Fix unconditional license print if H103 is not selected
    Regardless of H103 being selected by the user, if the license is not
    an exact apache2 license, it will be printed out poluting the output.
    
    With this change the comparison will be printed along with the error
    message, so will not be visible if H103 was not selected.
    
    Change-Id: Id526d83626e377aedd0bd2a998fc7d6e01058d3e

--- hacking/tests/checks/test_comments.py
+++ hacking/tests/checks/test_comments.py
@@ -90,7 +90,20 @@ class CoreTestCase(tests.TestCase):
     def test_H103_full_fail(self):
         """Verify that the H103 check finds an SPDX header"""
         self.assertEqual(
-            (2, 'H103: Header does not match Apache 2.0 License notice'),
+            (2, 'H103: Header does not match Apache 2.0 License notice\n'
+                '<license>!=<apache2>:\n'
+                '\'Licensed under the Apache License, Version 2.0 foo bar foo '
+                'bar foo bar foo bar foo bar\' !=\n'
+                '\'Licensed under the Apache License, Version 2.0 (the '
+                '"License"); you may not use this file except in compliance '
+                'with the License. You may obtain a copy of the License at '
+                'http://www.apache.org/licenses/LICENSE-2.0 Unless required '
+                'by applicable law or agreed to in writing, software '
+                'distributed under the License is distributed on an "AS IS" '
+                'BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either '
+                'express or implied. See the License for the specific '
+                'language governing permissions and limitations under the '
+                'License.\''),
             comments.hacking_has_correct_license(
                 None,
                 None,
